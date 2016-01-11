13ef220e2a1292b05729c539445450a7374b666e
Soren Norrby <info@sorennorrby.com>
2016-01-11 11:26:18 +0100

    Initial commit

---
+++ ESSArch_TP_old/config/wsgi.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env /ESSArch/python27/bin/python
+# -*- coding: UTF-8 -*-
+'''
+    ESSArch Tools - ESSArch is an Electronic Preservation Platform
+    Copyright (C) 2005-2013  ES Solutions AB
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Contact information:
+    Web - http://www.essolutions.se
+    Email - essarch@essolutions.se
+'''
+
+"""
+WSGI config for ESSArch Tools project.
+
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
+
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
+
+"""
+import os, sys, platform
+
+# Activate virtualenv before any imports
+if platform.system() != 'Linux' :
+    activate_this = '/ESSArch/env/Scripts/activate_this.py'
+    execfile(activate_this, dict(__file__=activate_this))
+    sys.path.append('/ESSArch/etp')
+else:
+    sys.path.append('/ESSArch/etp') # append path
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
