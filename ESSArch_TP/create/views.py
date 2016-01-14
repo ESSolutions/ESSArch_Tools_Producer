720ed25fd02f974c100ff9401b6002e107d0a135
Soren Norrby <info@sorennorrby.com>
2016-01-14 13:38:24 +0100

    chunked upload model imported

--- ESSArch_TP/create/views.py
+++ ESSArch_TP/create/views.py
@@ -54,14 +54,14 @@ from django.core.files.storage import FileSystemStorage
 
 from chunked_upload.settings import MAX_BYTES
 #from chunked_upload.views import ChunkedUploadBaseView
-#from chunked_upload.models import ChunkedUpload
+from chunked_upload.models import ChunkedUpload
 from chunked_upload.response import Response
 from chunked_upload.constants import http_status, COMPLETE
 from chunked_upload.exceptions import ChunkedUploadError
 
 
 from config.settings import MEDIA_ROOT
-#from models import ETPupload
+from models import ETPupload
 
 # from ESSArch Tools
 from ip.models import InformationPackage
