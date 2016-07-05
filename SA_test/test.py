caeca498d4fef7cd5efee42132f98279efab053f
Sydarkivera <simon.nilsson@sydarkivera.se>
2016-07-05 09:49:02 +0200

    Generating XML from template with files made in linear time

--- SA_test/test.py
+++ SA_test/test.py
@@ -2,6 +2,8 @@
 # import os
 import sys
 from lxml import etree
+import os
+import hashlib
 
 
 # METS_NS = 'http://www.loc.gov/METS/'
@@ -34,6 +36,10 @@ from lxml import etree
 # print etree.tostring(mets, pretty_print=True)
 
 
+############## ((  INFO  )) ##############
+
+# value must NOT contaion space
+
 ############## ((  TODO  )) ##############
 
 # 1. sanitize all attributes and values
@@ -44,23 +50,26 @@ from lxml import etree
 
 
 # info = {"UUID": "aslfd24lkn2l34nl2", "AgentType": "ORGANIZATION"}
-info = {"xmlns:mets":"http://www.loc.gov/METS/",
-        "xmlns:ext":"ExtensionMETS",
-        "xmlns:xlink":"http://www.w3.org/1999/xlink",
-        "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance",
-        "xsi:schemaLocation":"http://www.loc.gov/METS/ http://xml.ra.se/e-arkiv/METS/CSPackageMETS.xsd ExtensionMETS http://xml.ra.se/e-arkiv/METS/CSPackageExtensionMETS.xsd",
-        "PROFILE":"http://xml.ra.se/e-arkiv/METS/CommonSpecificationSwedenPackageProfile.xmll",
-        "LABEL":"Test of SIP 1",
-        "TYPE":"Personnel",
+info = {"xmlns:mets": "http://www.loc.gov/METS/",
+        "xmlns:ext": "ExtensionMETS",
+        "xmlns:xlink": "http://www.w3.org/1999/xlink",
+        "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+        "xsi:schemaLocation": "http://www.loc.gov/METS/ http://xml.ra.se/e-arkiv/METS/CSPackageMETS.xsd ExtensionMETS http://xml.ra.se/e-arkiv/METS/CSPackageExtensionMETS.xsd",
+        "PROFILE": "http://xml.ra.se/e-arkiv/METS/CommonSpecificationSwedenPackageProfile.xmll",
+        "LABEL": "Test of SIP 1",
+        "TYPE": "Personnel",
         # "mets:ID":"ID8b42fed2-c281-43b4-9798-5168126b0a4b",
-        "OBJID":"UUID:9bc10faa-3fff-4a8f-bf9a-638841061065",
-        "ext:CONTENTTYPESPECIFICATION":"FGS Personal, version 1",
-        "CREATEDATE":"2016-06-08T10:44:00+02:00",
-        "RECORDSTATUS":"NEW",
-        "ext:OAISTYPE":"SIP",
-        "agentName":"name",
-        "agentNote":"note",
-       }
+        "OBJID": "UUID:9bc10faa-3fff-4a8f-bf9a-638841061065",
+        "ext:CONTENTTYPESPECIFICATION": "FGS Personal, version 1",
+        "CREATEDATE": "2016-06-08T10:44:00+02:00",
+        "RECORDSTATUS": "NEW",
+        "ext:OAISTYPE": "SIP",
+        "agentName": "name",
+        "agentNote": "note",
+        "REFERENCECODE": "SE/RA/123456/24/F",
+        "SUBMISSIONAGREEMENT": "RA 13-2011/5329, 2012-04-12",
+        "MetsIdentifier": "sip.xml",
+        }
 
 # xmlFile = """
 # <mets min="1" max="1">
@@ -91,10 +100,13 @@ eol_ = '\n'
 
 debug = False
 pretty = True
+fileElements = []
+
 
 def dlog(string):
     if debug:
         print string
+
 
 def pretty_print(level, pretty):
     if pretty:
@@ -124,7 +136,7 @@ class xmlElement(object):
     dsf.
     '''
 
-    def __init__(self, tagName='', namespace = ''):
+    def __init__(self, tagName='', namespace=''):
         self.tagName = tagName
         self.children = []
         self.attributes = []
@@ -133,6 +145,8 @@ class xmlElement(object):
         self.karMax = -1
         self.namespace = namespace
         self.completeTagName = ''
+        self.containsFiles = False
+        self.printed = 0
         if self.namespace != '':
             self.completeTagName += self.namespace + ':'
         self.completeTagName += str(self.tagName)
@@ -145,21 +159,42 @@ class xmlElement(object):
         self.completeTagName += self.tagName
 
     def printXML(self, level=0):
-        pretty_print(level, pretty)
-        sys.stdout.write('<' + self.completeTagName)
-        for a in self.attributes:
-            a.printXML()
-        if self.children or self.value is not '':
-            sys.stdout.write('>' + eol_)
-            for child in self.children:
-                child.printXML(level+1)
-            if self.value is not '':
-                pretty_print(level+1, pretty)
-                sys.stdout.write(self.value + eol_)
+        # if self.containsFiles:
+            # print "contaions iles: " + self.tagName
+        if self.printed == 2:
+            return False
+        if self.printed == 0:
             pretty_print(level, pretty)
-            sys.stdout.write('</' + self.completeTagName + '>' + eol_)
+            sys.stdout.write('<' + self.completeTagName)
+            for a in self.attributes:
+                a.printXML()
+        if self.children or self.value is not '' or self.containsFiles:
+            if self.printed == 0:
+                sys.stdout.write('>' + eol_)
+            if not self.containsFiles or self.printed == 1:
+                for child in self.children:
+                    if child.printXML(level + 1):
+                        self.printed = 1
+                        # print "return True 1"
+                        return True
+                if self.value is not '':
+                    pretty_print(level + 1, pretty)
+                    sys.stdout.write(self.value + eol_)
+                pretty_print(level, pretty)
+                sys.stdout.write('</' + self.completeTagName + '>' + eol_)
+                self.printed = 2
+            else:
+                self.printed = 1
+                # print "return True 2: " + self.completeTagName
+                return True
         else:
             sys.stdout.write('/>' + eol_)
+            self.printed = 2
+
+    def printDebug(self):
+        print self.tagName
+        for child in self.children:
+            child.printDebug()
 
     def addAttribute(self, attribute):
         self.attributes.append(attribute)
@@ -169,41 +204,50 @@ class xmlElement(object):
             el.setNamespace(self.namespace)
         self.children.append(el)
 
-def create2(tree, namespace = ''):
+
+def create2(tree, namespace=''):
     empty = True
     hasAttrib = False
     t = xmlElement(tree.tag, namespace)
     if tree.text is not None:
-        if tree.text in info:
-            empty = False
-            t.value = info[tree.text]
-    for child in tree:
-        if child.tag == 'attr':
-            # value = info[child.text]
-            if child.get('value') is '1':
-                hasAttrib = True
-                t.addAttribute(xmlAttribute(child.get('name'), child.text))
-            else:
-                if child.text in info:
-                    hasAttrib = True
-                    value = info[child.text]
-                    name = child.get('name')
-                    t.addAttribute(xmlAttribute(name, value))
-                else:
-                    if child.get('req') == '1':
-                        print "ERROR: missing required value for: " + child.text
-                    else:
-                        # debug
-                        dlog("INFO: missing optional value for: " + child.text)
-
-        elif child.tag == 'namespace':
-            t.setNamespace(child.text)
-            namespace = child.text
-        else:
-            c = create2(child, namespace)
-            if c is not None:
+        text = tree.text.replace('\n', '').replace(' ', '')
+        if text != '':
+            # print tree.text.replace('\n', '').replace(' ','')
+            if text in info:
                 empty = False
-                t.addChild(c)
+                t.value = info[tree.text]
+    if tree.get('containsFiles') == '1':
+        t.containsFiles = True
+        empty = False
+        fileElements.append(tree)
+        # print "found files"
+    else:
+        for child in tree:
+            if child.tag == 'attr':
+                # value = info[child.text]
+                if child.get('value') is '1':
+                    hasAttrib = True
+                    t.addAttribute(xmlAttribute(child.get('name'), child.text))
+                else:
+                    if child.text in info:
+                        hasAttrib = True
+                        value = info[child.text]
+                        name = child.get('name')
+                        t.addAttribute(xmlAttribute(name, value))
+                    else:
+                        if child.get('req') == '1':
+                            print "ERROR: missing required value for: " + child.text
+                        else:
+                            # debug
+                            dlog("INFO: missing optional value for: " + child.text)
+            elif child.tag == 'namespace':
+                t.setNamespace(child.text)
+                namespace = child.text
+            else:
+                c = create2(child, namespace)
+                if c is not None:
+                    empty = False
+                    t.addChild(c)
     if empty:
         if tree.get('allowEmpty') == '1':
             return t
@@ -212,13 +256,130 @@ def create2(tree, namespace = ''):
     else:
         return t
 
+def parseFiles(filename='/SIP/arc/content'):
+    #'/SIP/sip test 1/metadata'
+    fileInfo = {"FName":""}
+    for dirname, dirnames, filenames in os.walk(filename):
+        # print dirname, dirnames, filenames
+        for file in filenames:
+
+            #populate dictionary
+
+            fileInfo['FName'] = file
+            fileInfo['FChecksum'] = hashlib.md5(filename+'/'+file).hexdigest() #get_sha256_hash(file_name)
+            # write to file
+            for e in fileElements:
+                t = create3(e, fileInfo)
+                t.printXML(3)
+
+def create3(el, fileInfo, namespace='mets'):
+    empty = True
+    t = xmlElement(el.tag, namespace)
+    # t.printXML()
+    # t.level = ?
+    # print fileInfo
+    if el.text is not None:
+        text = el.text.replace('\n', '').replace(' ', '')
+        if text != '':
+            # print tree.text.replace('\n', '').replace(' ','')
+            if text in fileInfo:
+                empty = False
+                t.value = fileInfo[el.text]
+    # if el.get('containsFiles') == '1':
+        # t.containsFiles = True
+        # empty = False
+        # print "found files"
+    # else:
+    for child in el:
+        if child.tag == 'attr':
+            # value = info[child.text]
+            if child.get('value') is '1':
+                t.addAttribute(xmlAttribute(child.get('name'), child.text))
+            else:
+                if child.text in fileInfo:
+                    value = fileInfo[child.text]
+                    name = child.get('name')
+                    t.addAttribute(xmlAttribute(name, value))
+                else:
+                    if child.get('req') == '1':
+                        print "ERROR: missing required value for: " + child.text
+                    else:
+                        # debug
+                        dlog("INFO: missing optional value for: " + child.text)
+        elif child.tag == 'namespace':
+            t.setNamespace(child.text)
+            namespace = child.text
+        else:
+            c = create3(child, fileInfo, namespace)
+            if c is not None:
+                empty = False
+                t.addChild(c)
+    if empty:
+        if el.get('allowEmpty') == '1':
+            return t
+        else:
+            return None
+    else:
+        return t
+
+
+
+# def create3(tree, namespace=''):
+#     empty = True
+#     hasAttrib = False
+#     t = xmlElement(tree.tag, namespace)
+#     if tree.text is not None:
+#         text = tree.text.replace('\n', '').replace(' ', '')
+#         if text != '':
+#             # print tree.text.replace('\n', '').replace(' ','')
+#             if text in info:
+#                 empty = False
+#                 t.value = info[tree.text]
+#     for child in tree:
+#         if child.tag == 'attr':
+#             # value = info[child.text]
+#             if child.get('value') is '1':
+#                 hasAttrib = True
+#                 t.addAttribute(xmlAttribute(child.get('name'), child.text))
+#             else:
+#                 if child.text in info:
+#                     hasAttrib = True
+#                     value = info[child.text]
+#                     name = child.get('name')
+#                     t.addAttribute(xmlAttribute(name, value))
+#                 else:
+#                     if child.get('req') == '1':
+#                         print "ERROR: missing required value for: " + child.text
+#                     else:
+#                         # debug
+#                         dlog("INFO: missing optional value for: " + child.text)
+#         elif child.tag == 'namespace':
+#             t.setNamespace(child.text)
+#             namespace = child.text
+#         elif child.tag == 'files':
+#             t.containsFiles = True
+#         else:
+#             c = create2(child, namespace)
+#             if c is not None:
+#                 empty = False
+#                 t.addChild(c)
+#     if empty:
+#         if tree.get('allowEmpty') == '1':
+#             return t
+#         else:
+#             return None
+#     else:
+#         return t
+
 # parser = etree.XMLParser(remove_blank_text=True)
 # pars = etree.XML(xmlFile, parser)
 
-pars = etree.parse("SA_test/template.xml")
+pars = etree.parse("template.xml")
 
 rootEl = create2(pars.getroot())
-rootEl.printXML()
+while rootEl.printXML():
+    parseFiles()
+    # print "files goes here \n\n\n" + str(rootEl.printed)
 
 # print etree.tostring(root)
 
