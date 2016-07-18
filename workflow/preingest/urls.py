aec150fe455b2e23e0060a0723217725c3dcc530
Oskar Persson <oskar.persson@polken.se>
2016-07-18 17:14:19 +0200

    Add view with json list of executed tasks

--- workflow/preingest/urls.py
+++ workflow/preingest/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^run/(?P<name>[a-zA-Z.]+)/$', views.run, name='run')
+    url(r'^run/(?P<name>[a-zA-Z.]+)/$', views.run, name='run'),
+    url(r'^tasks/$', views.tasks, name='tasks')
 ]
--- workflow/preingest/views.py
+++ workflow/preingest/views.py
@@ -5,10 +5,19 @@ from celery.result import AsyncResult
 from django.http import HttpResponse
 from django.template import loader
 
+from rest_framework.renderers import JSONRenderer
+
 from tasks import Sleepy
 
 from preingest.dbtask import DBTask
-from preingest.models import Task
+from preingest.models import ProcessStep, ProcessTask, Task
+from preingest.serializers import ProcessTaskSerializer
+
+class JSONResponse(HttpResponse):
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
 
 def index(request):
     template = loader.get_template('preingest/index.html')
@@ -22,3 +31,8 @@ def run(request, name, *args, **kwargs):
     [module, task] = name.rsplit('.', 1)
     getattr(importlib.import_module(module), task)().delay()
     return HttpResponse("Running {}".format(name), request)
+
+def tasks(request, *args, **kwargs):
+    tasks = ProcessTask.objects.all()
+    serializer = ProcessTaskSerializer(tasks, many=True)
+    return JSONResponse(serializer.data)
