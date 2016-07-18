a82e425cac809e7a9089c369f7d382d0e5bc3757
Oskar Persson <oskar.persson@polken.se>
2016-07-19 00:20:34 +0200

    Fix deserialization of task result

--- workflow/preingest/serializers.py
+++ workflow/preingest/serializers.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 
 from preingest.models import ProcessStep, ProcessTask
 
+class PickledObjectField(serializers.Field):
+    def to_representation(self, obj):
+        return obj
+
+    def to_internal_value(self, data):
+        return data
 
 class ProcessStepSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +17,5 @@ class ProcessStepSerializer(serializers.ModelSerializer):
 class ProcessTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProcessTask
+
+    result = PickledObjectField(allow_null=True, default=None)
