c4208e9db6ad5d9ab6e9d3cb3c63ffa36d143af2
Oskar Persson <oskar.persson@polken.se>
2016-07-19 18:28:04 +0200

    Add ability to run steps based on json files

--- workflow/preingest/urls.py
+++ workflow/preingest/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^run/(?P<name>[a-zA-Z.]+)/$', views.run, name='run'),
+    url(r'^run_step/(?P<name>[a-zA-Z.]+)/$', views.run_step, name='run_step'),
+    url(r'^run_task/(?P<name>[a-zA-Z.]+)/$', views.run_task, name='run_task'),
     url(r'^steps/$', views.steps, name='steps'),
     url(r'^tasks/$', views.tasks, name='tasks')
 ]
