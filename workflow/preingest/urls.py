2592cb252f0c5700b0ac1f51a9bcf91235401da9
Oskar Persson <oskar.persson@polken.se>
2016-07-22 01:07:51 +0200

    Add ability to undo only failed tasks in step

--- workflow/preingest/urls.py
+++ workflow/preingest/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     url(r'^tasks/$', views.tasks, name='tasks'),
     url(r'^history/$', views.history, name='history'),
     url(r'^history/(?P<step_id>[-\w]+)/$', views.history_detail, name='history_detail'),
-    url(r'^undo_task/(?P<processtask_id>[-\w]+)/$', views.undo_task, name='undo_task'),
+    url(r'^undo_failed/(?P<processstep_id>[-\w]+)/$', views.undo_failed, name='undo_failed'),
     url(r'^undo_step/(?P<processstep_id>[-\w]+)/$', views.undo_step, name='undo_step'),
     url(r'^retry_step/(?P<processstep_id>[-\w]+)/$', views.retry_step, name='retry_step'),
 ]
