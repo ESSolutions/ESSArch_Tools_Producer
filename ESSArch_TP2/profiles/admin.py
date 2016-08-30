058790d7fe494b4bdeafb2edda5a3e96fa5d0e72
Oskar <oskar.persson@polken.se>
2016-08-30 13:02:50 +0200

    Refactor profile models into a single model

--- ESSArch_TP2/profiles/admin.py
+++ ESSArch_TP2/profiles/admin.py
@@ -25,19 +25,20 @@
 from django.contrib import admin
 
 from profiles.models import (
-                             SubmissionAgreement, 
-                             ProfileTransferProject, ProfileTransferProjectRel, 
-                             ProfileContentType, ProfileContentTypeRel,  
-                             ProfileDataSelection, ProfileDataSelectionRel,
-                             ProfileClassification, ProfileClassificationRel, 
-                             ProfileImport, ProfileImportRel, 
-                             ProfileSubmitDescription, ProfileSubmitDescriptionRel,
-                             ProfileSIP, ProfileSIPRel,
-                             ProfileAIP, ProfileAIPRel,
-                             ProfileDIP, ProfileDIPRel, 
-                             ProfileWorkflow, ProfileWorkflowRel,
-                             ProfilePreservationMetadata, ProfilePreservationMetadataRel,
-                             )
+    SubmissionAgreement,
+    Profile,
+    ProfileTransferProjectRel,
+    ProfileContentTypeRel,
+    ProfileDataSelectionRel,
+    ProfileClassificationRel,
+    ProfileImportRel,
+    ProfileSubmitDescriptionRel,
+    ProfileSIPRel,
+    ProfileAIPRel,
+    ProfileDIPRel,
+    ProfileWorkflowRel,
+    ProfilePreservationMetadataRel,
+)
 
 class profile_transfer_project_Inline(admin.TabularInline):
     model = ProfileTransferProjectRel
@@ -198,373 +199,45 @@ class SubmissionAgreementAdmin( admin.ModelAdmin ):
 admin.site.register(SubmissionAgreement, SubmissionAgreementAdmin)
 
 """
-Profile Transfer Project
+Profile
 """
-class ProfileTransferProjectAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_transfer_project_name', 'profile_transfer_project_type', 'profile_transfer_project_status', 'profile_transfer_project_label' )
-    search_fields = ( 'profile_transfer_project_name', )
+class ProfileAdmin( admin.ModelAdmin ):
+    list_display = ( 'name', 'type', 'status', 'label' )
+    search_fields = ( 'name', )
     readonly_fields = ('id',)
-    list_filter = ('profile_transfer_project_name', 'profile_transfer_project_label')
+    list_filter = ('name', 'label')
     #fields = ('entity', 'value')
     fieldsets = (
                 (None,{
                    'classes': ('wide'),
                    'fields': (
-			      'id', #'profile_transfer_project',
-			      'profile_transfer_project_name',
-			      'profile_transfer_project_type',
-			      'profile_transfer_project_status',
-			      'profile_transfer_project_label',
+                              'id',
+                              'profile_type',
+                              'name',
+                              'type',
+                              'status',
+                              'label',
+                              'representation_info',
+                              'preservation_descriptive_info',
+                              'supplemental',
+                              'access_constraints',
+                              'datamodel_reference',
+                              'additional',
+                              'submission_method',
+                              'submission_schedule',
+                              'submission_data_inventory',
                               )}),
-                ('Transfer project specification and data',{
+                ('physcial and logical structure',{
                    'classes': ('collapse','wide'),
                    'fields': (
-                              'profile_transfer_project_specification',
-                              'profile_transfer_project_data',
+                              'structure',
+                              )}),
+                ('specification structure and data',{
+                   'classes': ('collapse','wide'),
+                   'fields': (
+                              'specification',
+                              'specification_data',
                               )}),
                 )
 
-admin.site.register(ProfileTransferProject, ProfileTransferProjectAdmin)
-
-
-"""
-Profile Content Type
-"""
-class ProfileContentTypeAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_content_type_name', 'profile_content_type_type', 'profile_content_type_status', 'profile_content_type_label' )
-    search_fields = ( 'profile_content_type_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_content_type_name', 'profile_content_type_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-			      'id', #'profile_content_type',
-			      'profile_content_type_name',
-			      'profile_content_type_type',
-			      'profile_content_type_status',
-			      'profile_content_type_label',
-                              )}),
-                ('Content type specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-			      'profile_content_type_specification',
-			      'profile_content_type_data',
-                              )}),
-                )
-
-admin.site.register(ProfileContentType, ProfileContentTypeAdmin)
-
-
-"""
-Profile Data Selection
-"""
-class ProfileDataSelectionAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_data_selection_name', 'profile_data_selection_type', 'profile_data_selection_status', 'profile_data_selection_label' )
-    search_fields = ( 'profile_data_selection_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_data_selection_name', 'profile_data_selection_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_data_selection',
-                              'profile_data_selection_name',
-                              'profile_data_selection_type',
-                              'profile_data_selection_status',
-                              'profile_data_selection_label',
-                              )}),
-                ('Data selection specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_data_selection_specification',
-                              'profile_data_selection_data',
-                              )}),
-                )
-
-admin.site.register(ProfileDataSelection, ProfileDataSelectionAdmin)
-
-
-"""
-Profile Classification
-"""
-class ProfileClassificationAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_classification_name', 'profile_classification_type', 'profile_classification_status', 'profile_classification_label' )
-    search_fields = ( 'profile_classification_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_classification_name', 'profile_classification_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_classification',
-                              'profile_classification_name',
-                              'profile_classification_type',
-                              'profile_classification_status',
-                              'profile_classification_label',
-                              )}),
-                ('Classification specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_classification_specification',
-                              'profile_classification_data',
-                              )}),
-                )
-
-admin.site.register(ProfileClassification, ProfileClassificationAdmin)
-
-
-"""
-Profile Import
-"""
-class ProfileImportAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_import_name', 'profile_import_type', 'profile_import_status', 'profile_import_label' )
-    search_fields = ( 'profile_import_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_import_name', 'profile_import_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_import',
-                              'profile_import_name',
-                              'profile_import_type',
-                              'profile_import_status',
-                              'profile_import_label',
-                              )}),
-                ('Import specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_import_specification',
-                              'profile_import_data',
-                              )}),
-                )
-
-admin.site.register(ProfileImport, ProfileImportAdmin)
-
-
-"""
-Profile Submit Description
-"""
-class ProfileSubmitDescriptionAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_sd_name', 'profile_sd_type', 'profile_sd_status', 'profile_sd_label' )
-    search_fields = ( 'profile_sd_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_sd_name', 'profile_sd_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_submit_description',
-                              'profile_sd_name',
-                              'profile_sd_type',
-                              'profile_sd_status',
-                              'profile_sd_label',
-                              )}),
-                ('Submit description specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_sd_specification',
-                              'profile_sd_data',
-                              )}),
-                )
-
-admin.site.register(ProfileSubmitDescription, ProfileSubmitDescriptionAdmin)
-
-
-"""
-Profile SIP (Submission Information Package)
-"""
-class ProfileSIPAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_sip_name', 'profile_sip_type', 'profile_sip_status', 'profile_sip_label' )
-    search_fields = ( 'profile_sip_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_sip_name', 'profile_sip_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_sip',
-                              'profile_sip_name',
-                              'profile_sip_type',
-                              'profile_sip_status',
-                              'profile_sip_label',
-                              'sip_representation_info',
-                              'sip_preservation_descriptive_info',
-                              'sip_supplemental',
-                              'sip_access_constraints',
-                              'sip_datamodel_reference',
-                              'sip_additional',
-                              'sip_submission_method',
-                              'sip_submission_schedule',
-                              'sip_submission_data_inventory',
-                              )}),
-                ('SIP physcial and logical structure',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'sip_structure',
-                              )}),
-                ('SIP specification structure and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'sip_specification',
-                              'sip_specification_data',
-                              )}),
-                )
-
-admin.site.register(ProfileSIP, ProfileSIPAdmin)
-
-
-"""
-Profile AIP (Archival Information Package)
-"""
-class ProfileAIPAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_aip_name', 'profile_aip_type', 'profile_aip_status', 'profile_aip_label' )
-    search_fields = ( 'profile_aip_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_aip_name', 'profile_aip_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_aip',
-                              'profile_aip_name',
-                              'profile_aip_type',
-                              'profile_aip_status',
-                              'profile_aip_label',
-                              'aip_representation_info',
-                              'aip_preservation_descriptive_info',
-                              'aip_supplemental',
-                              'aip_access_constraints',
-                              'aip_datamodel_reference',
-                              'aip_additional',
-                              'aip_submission_method',
-                              'aip_submission_schedule',
-                              'aip_submission_data_inventory',
-                              )}),
-                ('AIP physcial and logical structure',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'aip_structure',
-                              )}),
-                ('AIP specification structure and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'aip_specification',
-                              'aip_specification_data',
-                              )}),
-                )
-
-admin.site.register(ProfileAIP, ProfileAIPAdmin)
-
-
-"""
-Profile DIP (Dissemination Information Package)
-"""
-class ProfileDIPAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_dip_name', 'profile_dip_type', 'profile_dip_status', 'profile_dip_label' )
-    search_fields = ( 'profile_dip_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_dip_name', 'profile_dip_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_dip',
-                              'profile_dip_name',
-                              'profile_dip_type',
-                              'profile_dip_status',
-                              'profile_dip_label',
-                              'dip_representation_info',
-                              'dip_preservation_descriptive_info',
-                              'dip_supplemental',
-                              'dip_access_constraints',
-                              'dip_datamodel_reference',
-                              'dip_additional',
-                              'dip_submission_method',
-                              'dip_submission_schedule',
-                              'dip_submission_data_inventory',
-                              )}),
-                ('DIP physcial and logical structure',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'dip_structure',
-                              )}),
-                ('DIP specification structure and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'dip_specification',
-                              'dip_specification_data',
-                              )}),
-                )
-
-admin.site.register(ProfileDIP, ProfileDIPAdmin)
-
-
-"""
-Profile Workflow
-"""
-class ProfileWorkflowAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_workflow_name', 'profile_workflow_type', 'profile_workflow_status', 'profile_workflow_label' )
-    search_fields = ( 'profile_workflow_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_workflow_name', 'profile_workflow_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', #'profile_workflow',
-                              'profile_workflow_name',
-                              'profile_workflow_type',
-                              'profile_workflow_status',
-                              'profile_workflow_label',
-                              )}),
-                ('Workflow specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_workflow_specification',
-                              'profile_workflow_data',
-                              )}),
-                )
-
-admin.site.register(ProfileWorkflow, ProfileWorkflowAdmin)
-
-
-"""
-Profile Preservation Metadata
-"""
-class ProfilePreservationMetadataAdmin( admin.ModelAdmin ):
-    list_display = ( 'profile_preservation_metadata_name', 'profile_preservation_metadata_type', 'profile_preservation_metadata_status', 'profile_preservation_metadata_label' )
-    search_fields = ( 'profile_preservation_metadata_name', )
-    readonly_fields = ('id',)
-    list_filter = ('profile_preservation_metadata_name', 'profile_preservation_metadata_label')
-    #fields = ('entity', 'value')
-    fieldsets = (
-                (None,{
-                   'classes': ('wide'),
-                   'fields': (
-                              'id', 
-                              'profile_preservation_metadata_name',
-                              'profile_preservation_metadata_type',
-                              'profile_preservation_metadata_status',
-                              'profile_preservation_metadata_label',
-                              )}),
-                ('Preservation Metadata specification and data',{
-                   'classes': ('collapse','wide'),
-                   'fields': (
-                              'profile_preservation_metadata_specification',
-                              'profile_preservation_metadata_data',
-                              )}),
-                )
-
-admin.site.register(ProfilePreservationMetadata, ProfilePreservationMetadataAdmin)
-
+admin.site.register(Profile, ProfileAdmin)
