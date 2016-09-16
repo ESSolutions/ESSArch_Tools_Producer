5a2451a0049f3ffafb96c608124a1ba9e70c951a
petrus <petrus.jonsson@gmail.com>
2016-09-16 09:58:21 +0200

    fix small error

--- ESSArch_TP2/frontend/static/frontend/scripts/controllers/DropdownCtrl.js
+++ ESSArch_TP2/frontend/static/frontend/scripts/controllers/DropdownCtrl.js
@@ -1,10 +1,10 @@
-angular.module('myApp').controller('DropdownCtrl', function ($scope, $log, $rootScope) {
-  $scope.items = [
-    'Shortcut 1',
-    'Shortcut 2',
-    'Shortcut 3'
-  ];
-  $scope.editUserOptions = [
+angular.module('myApp').controller('DropdownCtrl', function ($scope, $log, $rootScope, $state, $stateParams) {
+    $scope.items = [
+        'Shortcut 1',
+        'Shortcut 2',
+        'Shortcut 3'
+    ];
+    $scope.editUserOptions = [
     {
         label: 'Edit profile',
         link: ''
@@ -17,27 +17,29 @@ angular.module('myApp').controller('DropdownCtrl', function ($scope, $log, $root
         label: 'Log in',
         link: 'login'
     }
-  ];
-  $scope.$watch(function() {
-      return $rootScope.auth.name;
-  }, function() {
-      $scope.name = $rootScope.auth.name;
-  }, true);
+    ];
+    $scope.$watch(function() {
+        return $rootScope.auth.name;
+    }, function() {
+        $scope.name = $rootScope.auth.name;
+    }, true);
 
+    $scope.gotoLink = function(choice) {
+        $state.go(choice.link)
+    };
+    $scope.status = {
+        isopen: false
+    };
 
-  $scope.status = {
-      isopen: false
-  };
+    $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+    };
 
-  $scope.toggled = function(open) {
-    $log.log('Dropdown is now: ', open);
-  };
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
 
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-  };
-
-  $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
+    $scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 });
