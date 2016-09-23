d2be66d84164849da308e454652332624d0be24c
petrus <petrus.jonsson@gmail.com>
2016-09-23 16:26:01 +0200

    Add smaller table rows, add refresh function, change tabs structure

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/TabsCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/TabsCtrl.js
@@ -1,22 +1,21 @@
-angular.module('myApp').controller('TabsCtrl', function TabsCtrl($scope, $location){
-  $scope.tabs = [
-      { link: 'home.myPage', label: 'My page' },
-      { link: 'home.createSip.info', label: 'Create SIP' },
-      { link: 'home.submitSip', label: 'Submit SIP' },
-     // { link : '#/recieve-SIP', label : 'Recieve SIP' }
+angular.module('myApp').controller('TabsCtrl', function TabsCtrl($scope, $location, $window){
+    $scope.tabs = [
+    { link: 'home.myPage', label: 'My page' },
+    { link: 'home.createSip.info', label: 'Create SIP' },
+    { link: 'home.submitSip', label: 'Submit SIP' },
+    // { link : '#/recieve-SIP', label : 'Recieve SIP' }
     ];
+    $scope.setActiveTab = function(index){
+        $window.localStorage.setItem("activeTab", index);
+    };
 
-  $scope.selectedTab = $scope.tabs[0];
-  $scope.setSelectedTab = function(tab) {
-    $scope.selectedTab = tab;
-  }
+    $scope.getActiveTab = function(){
+        return $window.localStorage.getItem("activeTab");
+    };
 
-  $scope.tabClass = function(tab) {
-    if ($scope.selectedTab == tab) {
-      return "active";
-    } else {
-      return "";
-    }
-  }
-  });
+    $scope.isActiveTab = function(tabName,index){
+        var activeTab = $scope.getActiveTab();
+        return (index === activeTab)
+    };
+});
 
