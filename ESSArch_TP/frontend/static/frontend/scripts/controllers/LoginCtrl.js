b3ef396dbc79fdca342c41de8bd59aed4a129741
petrus <petrus.jonsson@gmail.com>
2016-09-23 10:12:16 +0200

    Fix redirect to login

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/LoginCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/LoginCtrl.js
@@ -3,25 +3,25 @@ angular.module('myApp').controller('LoginCtrl', function ($scope, $location, myS
         $window.location.href="/admin/";
     }
     $scope.model = {'username':'','password':''};
-  	$scope.complete = false;
+    $scope.complete = false;
     $scope.login = function(formData){
-      $scope.errors = [];
-      Validate.form_validation(formData,$scope.errors);
-      if(!formData.$invalid){
-        djangoAuth.login($scope.model.username, $scope.model.password)
-        .then(function(data){
-        	// success case
-            djangoAuth.profile().then(function(data){
-                $rootScope.auth = data;
-                console.log($rootScope.auth);
-            });
-            $state.go('home');
-        },function(data){
-        	// error case
-        	$scope.errors = data;
-            console.log($scope.errors);
-        });
-      }
+        $scope.errors = [];
+        Validate.form_validation(formData,$scope.errors);
+        if(!formData.$invalid){
+            djangoAuth.login($scope.model.username, $scope.model.password)
+                .then(function(data){
+                    // success case
+                    djangoAuth.profile().then(function(data){
+                        $rootScope.auth = data;
+                        console.log($rootScope.auth);
+                    });
+                    $state.go('home');
+                },function(data){
+                    // error case
+                    $scope.errors = data;
+                    console.log($scope.errors);
+                });
+        }
     }
 });
 
