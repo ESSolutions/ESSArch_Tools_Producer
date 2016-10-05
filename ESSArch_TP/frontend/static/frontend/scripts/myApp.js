6033c22deb5c2342a19cea0c4801a73e1628c986
petrus <petrus.jonsson@gmail.com>
2016-10-05 13:29:54 +0200

    Move events controller to own file

--- ESSArch_TP/frontend/static/frontend/scripts/myApp.js
+++ ESSArch_TP/frontend/static/frontend/scripts/myApp.js
@@ -196,30 +196,6 @@ angular.module('myApp', ['ngRoute', 'treeControl', 'ui.bootstrap', 'formly', 'fo
             return permissions;
         }
 })
-.controller('pipeCtrl', ['Resource', function (service) {
-
-    var ctrl = this;
-
-    this.displayed = [];
-
-    this.callServer = function callServer(tableState) {
-
-        ctrl.isLoading = true;
-
-        var pagination = tableState.pagination;
-        console.log(pagination);
-        var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
-        var number = pagination.number || 10;  // Number of entries showed per page.
-        var pageNumber = start/number+1;
-
-        service.getPage(start, number, pageNumber, tableState).then(function (result) {
-            ctrl.displayed = result.data;
-            tableState.pagination.numberOfPages = result.numberOfPages;//set the number of pages so the pagination can update
-            ctrl.isLoading = false;
-        });
-    };
-
-}])
 .run(function(djangoAuth, $rootScope, $state, $location, $cookies, PermPermissionStore, PermRoleStore, $http, myService){
     djangoAuth.initialize('/rest-auth', false).then(function() {
 
