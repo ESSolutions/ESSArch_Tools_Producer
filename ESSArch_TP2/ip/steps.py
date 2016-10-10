16079bdba2850ba24e92990a63ac2a8300c15858
Oskar Persson <oskar.persson@polken.se>
2016-10-10 09:19:49 +0200

    Add missing file steps.py

---
+++ ESSArch_TP2/ip/steps.py
@@ -0,0 +1,32 @@
+from preingest.models import ProcessStep, ProcessTask
+
+
+def prepare_ip(label, responsible):
+    step = ProcessStep.objects.create(
+        name="Prepare IP",
+    )
+
+    t1 = ProcessTask.objects.create(
+        name="preingest.tasks.PrepareIP",
+        params={
+            "label": label,
+            "responsible": responsible,
+            "step": str(step.pk),
+        },
+        processstep_pos=0,
+    )
+
+    t2 = ProcessTask.objects.create(
+        name="preingest.tasks.CreateIPRootDir",
+        params={
+        },
+        result_params={
+            "information_package": t1.pk
+        },
+        processstep_pos=1,
+    )
+
+    step.tasks = [t1, t2]
+    step.save()
+
+    return step
