66ec6ccaa7b75d7d951be7be3d8d9e3f6b7e056e
petrus <petrus.jonsson@gmail.com>
2016-10-13 14:23:10 +0200

    Add modal support for task info on all relevant pages

---
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/ModalInstanceCtrl.js
@@ -0,0 +1,32 @@
+angular.module('myApp').controller('ModalInstanceCtrl', function ($uibModalInstance) {
+  var $ctrl = this;
+
+  $ctrl.save = function () {
+      $ctrl.data = {
+        name: $ctrl.profileName
+      };
+        $uibModalInstance.close($ctrl.data);
+  };
+  $ctrl.prepare = function () {
+      $ctrl.data = {
+        label: $ctrl.label
+      };
+        $uibModalInstance.close($ctrl.data);
+  };
+  $ctrl.lock = function () {
+      $ctrl.data = {
+          status: "locked"
+      }
+      $uibModalInstance.close($ctrl.data);
+  };
+  $ctrl.remove = function () {
+    $ctrl.data = {
+        status: "removed"
+    }
+    $uibModalInstance.close($ctrl.data);
+  };
+
+  $ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
