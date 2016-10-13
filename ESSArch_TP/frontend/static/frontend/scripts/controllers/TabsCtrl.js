577ad700f9af8a5c6b65002bd2303413256e6a81
petrus <petrus.jonsson@gmail.com>
2016-10-13 15:49:24 +0200

    Add proper 'activ-page' to tabs

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/TabsCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/TabsCtrl.js
@@ -1,4 +1,4 @@
-angular.module('myApp').controller('TabsCtrl', function TabsCtrl($scope, $location, $window){
+angular.module('myApp').controller('TabsCtrl', function TabsCtrl($state, $scope, $location, $window, myService){
     $scope.tabs = [
     { link: 'home.myPage', label: 'My page' },
     { link: 'home.createSip.info', label: 'Create SIP' },
@@ -6,19 +6,31 @@ angular.module('myApp').controller('TabsCtrl', function TabsCtrl($scope, $locati
     // { link : '#/recieve-SIP', label : 'Recieve SIP' }
     ];
 
-    $scope.setActiveTab = function (activeTab) {
-        sessionStorage.setItem("activeTab", activeTab);
+    $scope.is_active = function(tab) {
+        var isAncestorOfCurrentRoute = $state.includes(tab.link);
+        return isAncestorOfCurrentRoute;
     };
+    $scope.update_tabs = function() {
 
+        // sets which tab is active (used for highlighting)
+        angular.forEach($scope.tabs, function(tab, index) {
+            tab.params = tab.params || {};
+            tab.options = tab.options || {};
+            tab.class = tab.class || '';
+
+            tab.active = $scope.is_active(tab);
+            if (tab.active) {
+                $scope.tabs.active = index;
+            }
+        });
+    };
+    $scope.update_tabs();
     // Get active tab from localStorage
     $scope.getActiveTab = function () {
         return sessionStorage.getItem("activeTab");
     };
-
-    // Check if current tab is active
-    $scope.isActiveTab = function (tabName, index) {
-        var activeTab = $scope.getActiveTab();
-        return (activeTab === tabName || (activeTab === null && index === 0));
-    };
+    $scope.go = function(tab) {
+        $state.go(tab.link);
+    }
 });
 
