0633594cda9f6c5bf355f80846a115cf1c0d8907
petrus <petrus.jonsson@gmail.com>
2016-10-18 13:37:16 +0200

    Add language english, swedish and template for slovenian, static lang file load and cookie language variable

--- ESSArch_TP2/frontend/static/frontend/scripts/controllers/TabsCtrl.js
+++ ESSArch_TP2/frontend/static/frontend/scripts/controllers/TabsCtrl.js
@@ -1,10 +1,22 @@
-angular.module('myApp').controller('TabsCtrl', function TabsCtrl($state, $scope, $location, $window, myService){
-    $scope.tabs = [
-    { link: 'home.myPage', label: 'My page' },
-    { link: 'home.createSip.info', label: 'Create SIP' },
-    { link: 'home.submitSip', label: 'Submit SIP' },
-    // { link : '#/recieve-SIP', label : 'Recieve SIP' }
-    ];
+angular.module('myApp').controller('TabsCtrl', function TabsCtrl($state, $scope, $location, $window, myService, $translate, $rootScope){
+    $translate(['MYPAGE', 'CREATESIP', 'SUBMITSIP', 'RECIEVESIP']).then(function(translations) {
+        $scope.tabs = [
+        { link: 'home.myPage', label: translations.MYPAGE },
+        { link: 'home.createSip.info', label: translations.CREATESIP },
+        { link: 'home.submitSip', label: translations.SUBMITSIP },
+        // { link : 'home.recieveSip', label : translations.RECIEVESIP }
+        ];
+    });
+    $rootScope.$on('$translateChangeSuccess', function () {
+        $translate(['MYPAGE', 'CREATESIP', 'SUBMITSIP', 'RECIEVESIP']).then(function(translations) {
+            $scope.tabs = [
+            { link: 'home.myPage', label: translations.MYPAGE },
+            { link: 'home.createSip.info', label: translations.CREATESIP },
+            { link: 'home.submitSip', label: translations.SUBMITSIP },
+            // { link : 'home.recieveSip', label : translations.RECIEVESIP }
+            ];
+        });
+    });
 
     $scope.is_active = function(tab) {
         var isAncestorOfCurrentRoute = $state.includes(tab.link);
