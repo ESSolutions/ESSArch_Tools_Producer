1e4ff556402b8b175ddf9f922110d2b228c15990
petrus <petrus.jonsson@gmail.com>
2016-10-19 16:51:39 +0200

    Add Receive SIP with views, controllers and routs with basic design

---
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/FinalizeCtrl.js
@@ -0,0 +1,73 @@
+angular.module('myApp').controller('FinalizeCtrl', function($http, $scope, $rootScope, $state, $log, listViewService, Resource) {
+    /*******************************************/
+    /*Piping and Pagination for List-view table*/
+    /*******************************************/
+    var vm = this;
+    var ctrl = this;
+    this.itemsPerPage = 10;
+    $scope.selectedIp = {id: "", class: ""};
+    this.displayedIps = [];
+
+    //Get data according to ip table settings and populates ip table
+    this.callServer = function callServer(tableState) {
+    $scope.tableState = tableState;
+
+        var pagination = tableState.pagination;
+        var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+        var number = pagination.number;  // Number of entries showed per page.
+        var pageNumber = start/number+1;
+
+        Resource.getIpPage(start, number, pageNumber, tableState, $scope.selectedIp).then(function (result) {
+            console.log(tableState);
+            ctrl.displayedIps = result.data;
+            tableState.pagination.numberOfPages = result.numberOfPages;//set the number of pages so the pagination can update
+        });
+    };
+    //Make ip selected and add class to visualize
+    vm.displayedIps=[];
+    $scope.selectIp = function(row) {
+        vm.displayedIps.forEach(function(ip) {
+            if(ip.id == $scope.selectedIp.id){
+                ip.class = "";
+            }
+        });
+        if(row.id == $scope.selectedIp.id && !$scope.select && !$scope.statusShow && !$scope.eventShow){
+            $scope.selectedIp = {id: "", class: ""};
+        } else {
+            row.class = "selected";
+            $scope.selectedIp = row;
+        }
+    };
+
+    $scope.ipTableClick = function(row) {
+        $scope.ip = row;
+        console.log(row);
+        listViewService.getSa(row.SubmissionAgreement).then(function(sa) {
+            $scope.currentSa = sa;
+            console.log($scope.currentSa);
+        });
+    }
+
+    $scope.tabsEditView = [
+        {
+            label: "Package description",
+            templateUrl: 'static/frontend/views/reception_delivery_description.html'
+        },
+        {
+            label: "Content description",
+            templateUrl: "static/frontend/views/reception_package.html"
+        },
+        {
+            label: "Authority information",
+            templateUrl: "static/frontend/views/reception_package.html"
+        },
+        {
+            label: "Archival description",
+            templateUrl: "static/frontend/views/reception_package.html"
+        },
+        {
+            label: "Manage content",
+            templateUrl: "static/frontend/views/reception_package.html"
+        }
+    ];
+});
