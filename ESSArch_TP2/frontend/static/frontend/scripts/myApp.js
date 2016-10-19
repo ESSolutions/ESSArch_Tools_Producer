2be7e09eb6f6e44f94bd6717e3cacacca265d198
petrus <petrus.jonsson@gmail.com>
2016-10-19 16:51:39 +0200

    Add Receive SIP with views, controllers and routs with basic design

--- ESSArch_TP2/frontend/static/frontend/scripts/myApp.js
+++ ESSArch_TP2/frontend/static/frontend/scripts/myApp.js
@@ -1,182 +1,222 @@
 angular.module('myApp', ['ngRoute', 'treeControl', 'ui.bootstrap', 'formly', 'formlyBootstrap', 'smart-table', 'treeGrid', 'ui.router', 'ngCookies', 'permission', 'pascalprecht.translate', 'ngSanitize'])
     .config(function($routeProvider, formlyConfigProvider, $stateProvider, $urlRouterProvider, $rootScopeProvider, $uibTooltipProvider) {
-    $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: '/static/frontend/views/home.html',
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/static/frontend/views/home.html',
+            })
+        .state('login', {
+            url: '/login',
+            templateUrl: '/static/frontend/views/login.html',
+            controller: 'LoginCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
         })
-    .state('login', {
-        url: '/login',
-        templateUrl: '/static/frontend/views/login.html',
-        controller: 'LoginCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
+        .state('logout', {
+            url: '/logout',
+            templateUrl: '/static/frontend/views/logout.html',
+            controller: 'LogoutCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.myPage', {
+            url: 'my-page',
+            templateUrl: '/static/frontend/views/my_page.html',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip', {
+            url: 'create-SIP',
+            templateUrl: '/static/frontend/views/create_sip.html',
+            redirectTo: 'info',
+            controller: 'CreateSipCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.info', {
+            url: '/info',
+            templateUrl: '/static/frontend/views/create_sip_info.html',
+            controller: 'InfoCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.prepareIp', {
+            url: '/prepare-IP',
+            templateUrl: '/static/frontend/views/create_sip_prepare_ip.html',
+            controller: 'PrepareIpCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.dataSelection', {
+            url: '/data-selection',
+            templateUrl: '/static/frontend/views/create_sip_data_selection.html',
+            controller: 'PrepareIpCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.dataExtraction', {
+            url: '/data-extraction',
+            templateUrl: '/static/frontend/views/create_sip_data_extraction.html',
+            controller: 'PrepareIpCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.manageData', {
+            url: '/manage-data',
+            templateUrl: '/static/frontend/views/create_sip_manage_data.html',
+            controller: 'PrepareIpCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.createSip.ipApproval', {
+            url: '/create-SIP',
+            templateUrl: '/static/frontend/views/create_sip_ip_approval.html',
+            controller: 'IpApprovalCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.submitSip', {
+            url: 'submit-SIP',
+            templateUrl: '/static/frontend/views/submit_sip.html',
+            controller: 'IpApprovalCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.submitSip.prepareSip', {
+            url: '/prepare-SIP',
+            templateUrl: '/static/frontend/views/submit_sip_prepare_sip.html',
+            controller: 'PrepareSipCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.submitSip.reuseSip', {
+            url: '/reuse-SIP',
+            templateUrl: '/static/frontend/views/submit_sip_reuse_sip.html',
+            controller: 'IpApprovalCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.submitSip.removeSip', {
+            url: '/remove-SIP',
+            templateUrl: '/static/frontend/views/submit_sip_remove_sip.html',
+            controller: 'IpApprovalCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.receiveSip', {
+            url: 'receive-SIP',
+            templateUrl: '/static/frontend/views/recieve_sip.html',
+            controller: 'IpApprovalCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.receiveSip.reception', {
+            url: '/reception',
+            templateUrl: '/static/frontend/views/receive_sip_reception.html',
+            controller: 'ReceptionCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.receiveSip.finalize', {
+            url: '/finalize',
+            templateUrl: '/static/frontend/views/receive_sip_finalize.html',
+            controller: 'FinalizeCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.receiveSip.catalogue', {
+            url: '/catalogue',
+            templateUrl: '/static/frontend/views/receive_sip_catalogue.html',
+            controller: 'CatalogueCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('home.receiveSip.transferSip', {
+            url: '/transfer-SIP',
+            templateUrl: '/static/frontend/views/receive_sip_transfer_sip.html',
+            controller: 'TransferSipCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('restricted', {
+            url: '/restricted',
+            templateUrl: '/static/frontend/views/restricted.html',
+            controller: 'RestrictedCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        })
+        .state('authRequired', {
+            url: '/auth-required',
+            templateUrl: '/static/frontend/views/auth_required.html',
+            controller: 'authRequiredCtrl as vm',
+            resolve: {
+                authenticated: ['djangoAuth', function(djangoAuth){
+                    return djangoAuth.authenticationStatus();
+                }],
+            }
+        });
+        $urlRouterProvider.otherwise('/');
     })
-    .state('logout', {
-        url: '/logout',
-        templateUrl: '/static/frontend/views/logout.html',
-        controller: 'LogoutCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.myPage', {
-        url: 'my-page',
-        templateUrl: '/static/frontend/views/my_page.html',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip', {
-        url: 'create-SIP',
-        templateUrl: '/static/frontend/views/create_sip.html',
-        redirectTo: 'createSip.info',
-        controller: 'CreateSipCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.info', {
-        url: '/info',
-        templateUrl: '/static/frontend/views/create_sip_info.html',
-        controller: 'InfoCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.prepareIp', {
-        url: '/prepare-IP',
-        templateUrl: '/static/frontend/views/create_sip_prepare_ip.html',
-        controller: 'PrepareIpCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.dataSelection', {
-        url: '/data-selection',
-        templateUrl: '/static/frontend/views/create_sip_data_selection.html',
-        controller: 'PrepareIpCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.dataExtraction', {
-        url: '/data-extraction',
-        templateUrl: '/static/frontend/views/create_sip_data_extraction.html',
-        controller: 'PrepareIpCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.manageData', {
-        url: '/manage-data',
-        templateUrl: '/static/frontend/views/create_sip_manage_data.html',
-        controller: 'PrepareIpCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.createSip.ipApproval', {
-        url: '/create-SIP',
-        templateUrl: '/static/frontend/views/create_sip_ip_approval.html',
-        controller: 'IpApprovalCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.submitSip', {
-        url: 'submit-SIP',
-        templateUrl: '/static/frontend/views/submit_sip.html',
-        controller: 'IpApprovalCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.submitSip.prepareSip', {
-        url: '/prepare-SIP',
-        templateUrl: '/static/frontend/views/submit_sip_prepare_sip.html',
-        controller: 'PrepareSipCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.submitSip.reuseSip', {
-        url: '/reuse-SIP',
-        templateUrl: '/static/frontend/views/submit_sip_reuse_sip.html',
-        controller: 'IpApprovalCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.submitSip.removeSip', {
-        url: '/remove-SIP',
-        templateUrl: '/static/frontend/views/submit_sip_remove_sip.html',
-        controller: 'IpApprovalCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('home.recieveSip', {
-        url: '/recieve-SIP',
-        templateUrl: '/static/frontend/views/recieve_sip.html',
-        controller: 'IpApprovalCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('restricted', {
-        url: '/restricted',
-        templateUrl: '/static/frontend/views/restricted.html',
-        controller: 'RestrictedCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    })
-    .state('authRequired', {
-        url: '/auth-required',
-        templateUrl: '/static/frontend/views/auth_required.html',
-        controller: 'authRequiredCtrl as vm',
-        resolve: {
-          authenticated: ['djangoAuth', function(djangoAuth){
-            return djangoAuth.authenticationStatus();
-          }],
-        }
-    });
-    $urlRouterProvider.otherwise('/');
-})
 .config(['$httpProvider', function($httpProvider, $rootScope) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
