dc7698955ab07450c882a97fe04e75ed0ad85203
Oskar Persson <oskar.persson@polken.se>
2016-11-08 00:00:59 +0100

    Use id to filter ip using nav menu

--- ESSArch_TP/ip/filters.py
+++ ESSArch_TP/ip/filters.py
@@ -8,10 +8,10 @@ from ESSArch_Core.ip.models import InformationPackage
 class InformationPackageFilter(django_filters.FilterSet):
     state = ListFilter(name='State')
 
-    archival_institution = django_filters.CharFilter(name="ArchivalInstitution__name")
-    archivist_organization = django_filters.CharFilter(name='ArchivistOrganization__name')
-    archival_type = django_filters.CharFilter(name='ArchivalType__name')
-    archival_location = django_filters.CharFilter(name='ArchivalLocation__name')
+    archival_institution = django_filters.UUIDFilter(name="ArchivalInstitution__pk")
+    archivist_organization = django_filters.UUIDFilter(name='ArchivistOrganization__pk')
+    archival_type = django_filters.UUIDFilter(name='ArchivalType__pk')
+    archival_location = django_filters.UUIDFilter(name='ArchivalLocation__pk')
 
     class Meta:
         model = InformationPackage
