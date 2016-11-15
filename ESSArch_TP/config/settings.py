4a401725447981087ab66553271e5e0690b0533f
Oskar Persson <oskar.persson@polken.se>
2016-11-15 10:17:39 +0100

    Set SESSION_COOKIE_NAME to 'etp'

--- ESSArch_TP/config/settings.py
+++ ESSArch_TP/config/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'ze7xnd#&9_m)05j&j8wpu!=dp+jlj3olk&@k7amq9-s2x+b=$%'
+SESSION_COOKIE_NAME = 'etp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
