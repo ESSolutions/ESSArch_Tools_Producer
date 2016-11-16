237ca0bffbc3fba96940820994cc3be19d6a24c7
Oskar Persson <oskar.persson@polken.se>
2016-11-16 10:40:32 +0100

    Fix code formatting

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/AngularTreeCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/AngularTreeCtrl.js
@@ -13,139 +13,153 @@ angular.module('myApp').controller('AngularTreeCtrl', function AngularTreeCtrl($
             labelSelected: "a8"
         }
     }
-    $scope.ArchivalInstitution =
-        [
-        { "name" : "Archival institution", "children" : [
-        ]}
-        ];
-        $scope.ArchivistOrganization = [
-        { "name" : "Archivist organization", "children" : [
-            ]}
-        ];
-        $scope.ArchivalType = [
-        { "name" : "Archival type", "children" : [
-        ]}
-        ];
-        $scope.ArchivalLocation = [
-        { "name" : "Archival location", "children" : [
-        ]}
-        ];
-        $scope.other = [
-        { "name" : "other", "children" : [
-        ]}
-        ];
-        $scope.loadNavigation = function() {
-            $http({
-                method: 'GET',
-                url: appConfig.djangoUrl+"archival-institutions/"
-            }).then(function(response) {
-                $scope.ArchivalInstitution[0].children = response.data;
-            });
-            $http({
-                method: 'GET',
-                url: appConfig.djangoUrl+"archivist-organizations/"
-            }).then(function(response) {
-                $scope.ArchivistOrganization[0].children = response.data;
-            });
-            $http({
-                method: 'GET',
-                url: appConfig.djangoUrl+"archival-types/"
-            }).then(function(response) {
-                $scope.ArchivalType[0].children = response.data;
-            });
-            $http({
-                method: 'GET',
-                url: appConfig.djangoUrl+"archival-locations/"
-            }).then(function(response) {
-                $scope.ArchivalLocation[0].children = response.data;
-            });
+    $scope.ArchivalInstitution = [
+        {
+            "name": "Archival institution",
+            "children": []
         }
-        $scope.loadNavigation();
-        $rootScope.navigationFilter = {
-            institution: null,
-            organization: null,
-            type: null,
-            location: null,
-            other: null
-        };
+    ];
 
-        $scope.showSelectedInstitution = function(node) {
-            $scope.nodeOther = null;
-            $rootScope.navigationFilter.other = null;
-            if(angular.isUndefined(node.id)){
-                $rootScope.navigationFilter.institution = null;
-                return;
-            }
-            if($rootScope.navigationFilter.institution == node.id){
-                $rootScope.navigationFilter.institution = null;
-            } else {
-                $rootScope.navigationFilter.institution = node.id;
-            }
+    $scope.ArchivistOrganization = [
+        {
+            "name": "Archivist organization",
+            "children": []
         }
+    ];
 
-        $scope.showSelectedOrganization = function(node) {
-            $scope.nodeOther = null;
-            $rootScope.navigationFilter.other = null;
-            if(angular.isUndefined(node.id)){
-                $rootScope.navigationFilter.organization = null;
-                return;
-            }
-            if($rootScope.navigationFilter.organization == node.id) {
-                $rootScope.navigationFilter.organization = null;
-            } else {
-                $rootScope.navigationFilter.organization = node.id;
-            }
+    $scope.ArchivalType = [
+        {
+            "name": "Archival type",
+            "children": []
         }
+    ];
 
-        $scope.showSelectedType = function(node) {
-            $scope.nodeOther = null;
-            $rootScope.navigationFilter.other = null;
-            if(angular.isUndefined(node.id)){
-                $rootScope.navigationFilter.type = null;
-                return;
-            }
-            if($rootScope.navigationFilter.type == node.id) {
-                $rootScope.navigationFilter.type = null;
-            } else {
-                $rootScope.navigationFilter.type = node.id;
-            }
+    $scope.ArchivalLocation = [
+       {
+           "name": "Archival location",
+           "children": []
         }
+    ];
 
-        $scope.showSelectedLocation = function(node) {
-            $scope.nodeOther = null;
-            $rootScope.navigationFilter.other = null;
-           if(angular.isUndefined(node.id)){
-                $rootScope.navigationFilter.location = null;
-                return;
-            }
-           if($rootScope.navigationFilter.location == node.id) {
-                $rootScope.navigationFilter.location = null;
-           } else {
-                $rootScope.navigationFilter.location = node.id;
-           }
+    $scope.other = [
+        {
+            "name": "other",
+            "children": []
         }
+    ];
 
-        $scope.showSelectedOther = function(node) {
-            $scope.nodeInst = null;
-            $scope.nodeOrg = null;
-            $scope.nodeType = null;
-            $scope.nodeLoc = null;
-            if($rootScope.navigationFilter.other) {
-                $rootScope.navigationFilter = {
-                    institution: null,
-                    organization: null,
-                    type: null,
-                    location: null,
-                    other: null
-                };
-            } else {
-                $rootScope.navigationFilter = {
-                    institution: null,
-                    organization: null,
-                    type: null,
-                    location: null,
-                    other: true
-                };
-            }
+    $scope.loadNavigation = function() {
+        $http({
+            method: 'GET',
+            url: appConfig.djangoUrl+"archival-institutions/"
+        }).then(function(response) {
+            $scope.ArchivalInstitution[0].children = response.data;
+        });
+        $http({
+            method: 'GET',
+            url: appConfig.djangoUrl+"archivist-organizations/"
+        }).then(function(response) {
+            $scope.ArchivistOrganization[0].children = response.data;
+        });
+        $http({
+            method: 'GET',
+            url: appConfig.djangoUrl+"archival-types/"
+        }).then(function(response) {
+            $scope.ArchivalType[0].children = response.data;
+        });
+        $http({
+            method: 'GET',
+            url: appConfig.djangoUrl+"archival-locations/"
+        }).then(function(response) {
+            $scope.ArchivalLocation[0].children = response.data;
+        });
+    }
+    $scope.loadNavigation();
+    $rootScope.navigationFilter = {
+        institution: null,
+        organization: null,
+        type: null,
+        location: null,
+        other: null
+    };
+
+    $scope.showSelectedInstitution = function(node) {
+        $scope.nodeOther = null;
+        $rootScope.navigationFilter.other = null;
+        if(angular.isUndefined(node.id)){
+            $rootScope.navigationFilter.institution = null;
+            return;
         }
+        if($rootScope.navigationFilter.institution == node.id){
+            $rootScope.navigationFilter.institution = null;
+        } else {
+            $rootScope.navigationFilter.institution = node.id;
+        }
+    }
+
+    $scope.showSelectedOrganization = function(node) {
+        $scope.nodeOther = null;
+        $rootScope.navigationFilter.other = null;
+        if(angular.isUndefined(node.id)){
+            $rootScope.navigationFilter.organization = null;
+            return;
+        }
+        if($rootScope.navigationFilter.organization == node.id) {
+            $rootScope.navigationFilter.organization = null;
+        } else {
+            $rootScope.navigationFilter.organization = node.id;
+        }
+    }
+
+    $scope.showSelectedType = function(node) {
+        $scope.nodeOther = null;
+        $rootScope.navigationFilter.other = null;
+        if(angular.isUndefined(node.id)){
+            $rootScope.navigationFilter.type = null;
+            return;
+        }
+        if($rootScope.navigationFilter.type == node.id) {
+            $rootScope.navigationFilter.type = null;
+        } else {
+            $rootScope.navigationFilter.type = node.id;
+        }
+    }
+
+    $scope.showSelectedLocation = function(node) {
+        $scope.nodeOther = null;
+        $rootScope.navigationFilter.other = null;
+       if(angular.isUndefined(node.id)){
+            $rootScope.navigationFilter.location = null;
+            return;
+        }
+       if($rootScope.navigationFilter.location == node.id) {
+            $rootScope.navigationFilter.location = null;
+       } else {
+            $rootScope.navigationFilter.location = node.id;
+       }
+    }
+
+    $scope.showSelectedOther = function(node) {
+        $scope.nodeInst = null;
+        $scope.nodeOrg = null;
+        $scope.nodeType = null;
+        $scope.nodeLoc = null;
+        if($rootScope.navigationFilter.other) {
+            $rootScope.navigationFilter = {
+                institution: null,
+                organization: null,
+                type: null,
+                location: null,
+                other: null
+            };
+        } else {
+            $rootScope.navigationFilter = {
+                institution: null,
+                organization: null,
+                type: null,
+                location: null,
+                other: true
+            };
+        }
+    }
 });
