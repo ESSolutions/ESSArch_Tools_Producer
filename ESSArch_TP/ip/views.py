fa05879ab7ba0abcdd7402a3f12ef6d917643fb1
Oskar Persson <oskar.persson@polken.se>
2016-11-24 14:53:27 +0100

    Set outcome when creating custom events

--- ESSArch_TP/ip/views.py
+++ ESSArch_TP/ip/views.py
@@ -283,6 +283,7 @@ class EventIPViewSet(viewsets.ModelViewSet):
         """
 
         outcomeDetailNote = request.data.get('eventOutcomeDetailNote', None)
+        outcome = request.data.get('eventOutcome', 0)
         type_id = request.data.get('eventType', None)
         ip_id = request.data.get('information_package', None)
 
@@ -290,7 +291,7 @@ class EventIPViewSet(viewsets.ModelViewSet):
         ip = InformationPackage.objects.get(pk=ip_id)
 
         EventIP.objects.create(
-            eventOutcomeDetailNote=outcomeDetailNote, eventType=eventType,
-            linkingObjectIdentifierValue=ip
+            eventOutcome=outcome, eventOutcomeDetailNote=outcomeDetailNote,
+            eventType=eventType, linkingObjectIdentifierValue=ip
         )
         return Response({"status": "Created event"})
