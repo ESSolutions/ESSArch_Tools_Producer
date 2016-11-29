42b8f9c2e01c8b2713db53463da62026c1db5adb
Oskar Persson <oskar.persson@polken.se>
2016-11-29 10:42:38 +0100

    Fix 404 bug in IP table

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/IpApprovalCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/IpApprovalCtrl.js
@@ -167,7 +167,7 @@ angular.module('myApp').controller('IpApprovalCtrl', function ($log, $scope, myS
             var sorting = tableState.sort;
             var pagination = tableState.pagination;
             var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
-            var number = pagination.number;  // Number of entries showed per page.
+            var number = pagination.number || ctrl.itemsPerPage;  // Number of entries showed per page.
             var pageNumber = start/number+1;
 
             Resource.getIpPage(start, number, pageNumber, tableState, $scope.selectedIp, sorting, search, "Prepared,Creating,Created").then(function (result) {
