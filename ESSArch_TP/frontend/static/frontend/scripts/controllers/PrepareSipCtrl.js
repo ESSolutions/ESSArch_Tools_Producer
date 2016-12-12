4559eb6a81e222ae352c8453262934e07982110c
petrus <petrus.jonsson@gmail.com>
2016-12-12 11:45:11 +0100

    Fix typo

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/PrepareSipCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/PrepareSipCtrl.js
@@ -518,7 +518,7 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
                 $scope.getListViewData();
                 updateListViewConditional();
             }, 1000);
-            $scope.submitDisabled = true;
+            $scope.submitDisabled = false;;
         }, function(response) {
             console.log(response.status);
         });
