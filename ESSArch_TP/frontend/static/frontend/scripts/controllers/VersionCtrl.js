c9a6351680f5af2a8cbc090c45d018863a900c7c
petrus <petrus.jonsson@gmail.com>
2017-01-12 14:24:05 +0100

    Add contact with redirect to essolutions.se

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/VersionCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/VersionCtrl.js
@@ -1,5 +1,8 @@
-angular.module('myApp').controller('VersionCtrl', function($scope, myService) {
+angular.module('myApp').controller('VersionCtrl', function($scope, myService, $window) {
     myService.getVersionInfo().then(function(result) {
         $scope.sysInfo = result;
     });
+    $scope.redirectToEss = function(){
+        $window.open('http://www.essolutions.se', '_blank');
+    };
 });
