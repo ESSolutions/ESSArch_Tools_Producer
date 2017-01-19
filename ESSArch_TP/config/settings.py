ff0caaec3a5616b4c26ef04c284ce1ec289e5c9e
Henrik Ek <henrik@essolutions.se>
2017-01-19 15:14:10 +0100

    Change from sqlite to mysql in settings.py

--- ESSArch_TP/config/settings.py
+++ ESSArch_TP/config/settings.py
@@ -103,11 +103,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        #'STORAGE_ENGINE': 'MyISAM',           # STORAGE_ENGINE for MySQL database tables, 'MyISAM' or 'INNODB'
+        'NAME': 'etp',                    # Or path to database file if using sqlite3.
+        'USER': 'arkiv',                      # Not used with sqlite3.
+        'PASSWORD': 'password',               # Not used with sqlite3.
+        'HOST': '',                           # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                           # Set to empty string for default. Not used with sqlite3.
+        # This options for storage_engine have to be set for "south migrate" to work.
+        'OPTIONS': {
+           "init_command": "SET storage_engine=MyISAM",
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
