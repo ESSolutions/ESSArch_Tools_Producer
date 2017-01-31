f73ac40e8a6942b81bd8c79a906cc9bf1c210b6f
Oskar Persson <oskar.persson@polken.se>
2017-01-31 10:19:46 +0100

    Remove duplicate licenses

--- ESSArch_TP/install/install_profiles_etp_sydarkivera.py
+++ ESSArch_TP/install/install_profiles_etp_sydarkivera.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env /ESSArch/python27/bin/python
+# -*- coding: UTF-8 -*-
+
 """
     ESSArch is an open source archiving and digital preservation system
 
@@ -20,36 +23,6 @@
     Contact information:
     Web - http://www.essolutions.se
     Email - essarch@essolutions.se
-"""
-
-#!/usr/bin/env /ESSArch/python27/bin/python
-# -*- coding: UTF-8 -*-
-'''
-    ESSArch is an OAIS Electronic Archiving Solution
-
-    ESSArch Tools for Producer (ETP)
-    Copyright (C) 2005-2017  ES Solutions AB
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-    Contact information:
-    Web - http://www.essolutions.se
-    Email - essarch@essolutions.se
-'''
-
-"""
-    Install script - Sydarkivera
 """
 
 import json
