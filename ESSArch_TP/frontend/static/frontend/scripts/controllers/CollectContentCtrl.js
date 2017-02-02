31477131a088b6ed87a50ccba28d290d69ce06c0
petrus <petrus.jonsson@gmail.com>
2017-02-02 11:58:36 +0100

    Move code to base controller to avoid duplicate code Most of the status view code is moved, except intervals and watcher for state change

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/CollectContentCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/CollectContentCtrl.js
@@ -22,7 +22,8 @@
     Email - essarch@essolutions.se
 */
 
-angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModal, $timeout, $scope, $rootScope, $window, $location, $sce, $http, myService, appConfig, $state, $stateParams, listViewService, $interval, Resource, $q, $translate, $anchorScroll, PermPermissionStore, $cookies) {
+angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModal, $timeout, $scope, $rootScope, $window, $location, $sce, $http, myService, appConfig, $state, $stateParams, listViewService, $interval, Resource, $q, $translate, $anchorScroll, PermPermissionStore, $cookies, $controller) {
+    $controller('BaseCtrl', { $scope: $scope });
     var vm = this;
     var ipSortString = "Prepared,Uploading";
     vm.itemsPerPage = $cookies.get('etp-ips-per-page') || 10;
@@ -31,119 +32,6 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
     };
     vm.flowDestination = null;
     // List view
-    //Go to give state
-    $scope.changePath= function(path) {
-        myService.changePath(path);
-    };
-    //Redirect to django admin page
-    $scope.redirectAdmin = function () {
-        $window.location.href="/admin/";
-    }
-    $scope.tree_data = [];
-    $scope.angular = angular;
-    $translate(['LABEL', 'RESPONSIBLE', 'DATE', 'STATE', 'STATUS']).then(function(translations) {
-        $scope.responsible = translations.RESPONSIBLE;
-        $scope.label = translations.LABEL;
-        $scope.date = translations.DATE;
-        $scope.state = translations.STATE;
-        $scope.status = translations.STATUS;
-        $scope.expanding_property = {
-            field: "name",
-            displayName: $scope.label,
-        };
-        $scope.col_defs = [
-        {
-            field: "user",
-            displayName: $scope.responsible,
-        },
-        {
-            cellTemplate: "<div ng-include src=\"'static/frontend/views/task_pagination.html'\"></div>"
-        },
-        {
-            field: "time_started",
-            displayName: $scope.date
-        },
-        {
-            field: "status",
-            displayName: $scope.state,
-            cellTemplate: "<div ng-if=\"row.branch[col.field] == 'SUCCESS'\" class=\"step-state-success\"><b>{{'SUCCESS' | translate}}</b></div><div ng-if=\"row.branch[col.field] == 'FAILURE'\" class=\"step-state-failure\"><b>{{'FAILURE' | translate}}</b></div><div ng-if=\"row.branch[col.field] != 'SUCCESS' && row.branch[col.field] !='FAILURE'\" class=\"step-state-in-progress\"><b>{{'INPROGRESS' | translate}}</b></div>"
-
-        },
-        {
-            field: "progress",
-            displayName: $scope.status,
-            cellTemplate: "<uib-progressbar class=\"progress\" value=\"row.branch[col.field]\" type=\"success\"><b>{{row.branch[col.field]+\"%\"}}</b></uib-progressbar>"
-        },
-        {
-            cellTemplate: "<div ng-include src=\"'static/frontend/views/undo_redo.html'\"></div>"
-        }
-        ];
-    });
-
-    $scope.myTreeControl = {};
-    $scope.myTreeControl.scope = this;
-    //Undo steps/tasks
-    $scope.myTreeControl.scope.taskStepUndo = function(branch) {
-        $http({
-            method: 'POST',
-            url: branch.url+"undo/"
-        }).then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }, function() {
-            console.log("error");
-        });
-    };
-    //Redo steps/tasks
-    $scope.myTreeControl.scope.taskStepRedo = function(branch){
-        $http({
-            method: 'POST',
-            url: branch.url+"retry/"
-        }).then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }, function() {
-            console.log("error");
-        });
-    };
-    $scope.currentStepTask = {id: ""}
-    $scope.myTreeControl.scope.updatePageNumber = function(branch, page) {
-        if(page > branch.page_number && branch.next){
-            branch.page_number = parseInt(branch.next.page);
-            listViewService.getChildrenForStep(branch, branch.page_number);
-        } else if(page < branch.page_number && branch.prev && page > 0) {
-            branch.page_number = parseInt(branch.prev.page);
-            listViewService.getChildrenForStep(branch, branch.page_number);
-        }
-    };
-    //Click on +/- on step
-    $scope.stepClick = function(step) {
-        listViewService.getChildrenForStep(step);
-    };
-
-    //Click funciton for steps and tasks
-    $scope.stepTaskClick = function(branch) {
-        if($scope.stepTaskInfoShow && $scope.currentStepTask.id == branch.id){
-            $scope.stepTaskInfoShow = false;
-        }else {
-            $scope.stepTaskInfoShow = true;
-            $http({
-                method: 'GET',
-                url: branch.url
-            }).then(function(response){
-                $scope.currentStepTask = response.data;
-                if(branch.flow_type == "task"){
-                    $scope.taskInfoModal();
-                } else {
-                    $scope.stepInfoModal();
-                }
-            }, function(response) {
-                response.status;
-            });
-        }
-    };
      // Click funtion columns that does not have a relevant click function
      $scope.ipRowClick = function(row) {
          $scope.selectIp(row);
@@ -190,40 +78,6 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
          $interval.cancel(stateInterval);
          $interval.cancel(listViewInterval);
      });
-     //Get data for status view
-     function checkExpanded(nodes) {
-         var ret = [];
-         nodes.forEach(function(node) {
-             if(node.expanded == true) {
-                 ret.push(node);
-             }
-             if(node.children && node.children.length > 0) {
-                 ret = ret.concat(checkExpanded(node.children));
-             }
-         });
-         return ret;
-     }
-     //Update status view data
-    //Update status view data
-    $scope.statusViewUpdate = function(row){
-        $scope.statusLoading = true;
-        var expandedNodes = [];
-        if($scope.tree_data != []) {
-            expandedNodes = checkExpanded($scope.tree_data);
-        }
-        listViewService.getTreeData(row, expandedNodes).then(function(value) {
-            $scope.tree_data = value;
-            $scope.statusLoading = false;
-        }, function(response){
-            if(response.status == 404) {
-                $scope.statusShow = false;
-                $timeout(function(){
-                    $scope.getListViewData();
-                    updateListViewConditional();
-                }, 1000);
-            }
-        });
-    };
 
      /*******************************************/
      /*Piping and Pagination for List-view table*/
@@ -328,38 +182,7 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
     //$interval(function(){$scope.getListViewData();}, 5000, false);
 
     //Creates and shows modal with task information
-    $scope.taskInfoModal = function () {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/task_info_modal.html',
-            scope: $scope,
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data, $ctrl) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
-    //Creates and shows modal with step information
-    $scope.stepInfoModal = function () {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/step_info_modal.html',
-            scope: $scope,
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        });
-        modalInstance.result.then(function (data, $ctrl) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
-    $scope.max = 100;
+   $scope.max = 100;
     //Get data for eventlog view
     function getEventlogData() {
         listViewService.getEventlogData().then(function(value){
@@ -418,22 +241,6 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
     $scope.eventlog = false;
     $scope.yes = $translate.instant('YES');
     $scope.no = $translate.instant('NO');
-    //Create and show modal for remove ip
-    $scope.removeIpModal = function (ipObject) {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/remove-ip-modal.html',
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data) {
-            $scope.removeIp(ipObject);
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
     //Remove and ip
     $scope.removeIp = function (ipObject) {
         $http({
@@ -447,23 +254,6 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
             $scope.eventShow = false;
             $scope.statusShow = false;
             $rootScope.loadNavigation(ipSortString);
-        });
-    }
-    $scope.tracebackModal = function (profiles) {
-        $scope.profileToSave = profiles;
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/task_traceback_modal.html',
-            scope: $scope,
-            size: 'lg',
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
         });
     }
     //UPLOAD
@@ -544,8 +334,5 @@ angular.module('myApp').controller('CollectContentCtrl', function($log, $uibModa
     $scope.fileTransferFilter = function(file)
     {
         return file.isUploading();
-    };
-    $scope.checkPermission = function(permissionName) {
-        return !angular.isUndefined(PermPermissionStore.getPermissionDefinition(permissionName));
     };
 });
--- ESSArch_TP/frontend/static/frontend/scripts/controllers/PrepareSipCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/PrepareSipCtrl.js
@@ -22,130 +22,16 @@
     Email - essarch@essolutions.se
 */
 
-angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, $timeout, $scope, $rootScope, $window, $location, $sce, $http, myService, appConfig, $state, $stateParams, listViewService, $interval, Resource, $q, $translate, $anchorScroll, PermPermissionStore, $cookies){
+angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, $timeout, $scope, $rootScope, $window, $location, $sce, $http, myService, appConfig, $state, $stateParams, listViewService, $interval, Resource, $q, $translate, $anchorScroll, PermPermissionStore, $cookies, $controller){
+    $controller('BaseCtrl', { $scope: $scope });
     var vm = this;
     var ipSortString = "Created,Submitting,Submitted";
     vm.itemsPerPage = $cookies.get('etp-ips-per-page') || 10;
     $scope.updateIpsPerPage = function(items) {
         $cookies.put('etp-ips-per-page', items);
     };
-
     // List view
-    //Go to give state
-    $scope.changePath= function(path) {
-        myService.changePath(path);
-    };
-    //Redirect to django admin page
-    $scope.redirectAdmin = function () {
-        $window.location.href="/admin/";
-    }
-    $scope.tree_data = [];
-    $scope.angular = angular;
-    $translate(['LABEL', 'RESPONSIBLE', 'DATE', 'STATE', 'STATUS']).then(function(translations) {
-        $scope.responsible = translations.RESPONSIBLE;
-        $scope.label = translations.LABEL;
-        $scope.date = translations.DATE;
-        $scope.state = translations.STATE;
-        $scope.status = translations.STATUS;
-        $scope.expanding_property = {
-            field: "name",
-            displayName: $scope.label,
-        };
-        $scope.col_defs = [
-        {
-            field: "user",
-            displayName: $scope.responsible,
-        },
-        {
-            cellTemplate: "<div ng-include src=\"'static/frontend/views/task_pagination.html'\"></div>"
-        },
-        {
-            field: "time_started",
-            displayName: $scope.date
-        },
-        {
-            field: "status",
-            displayName: $scope.state,
-            cellTemplate: "<div ng-if=\"row.branch[col.field] == 'SUCCESS'\" class=\"step-state-success\"><b>{{'SUCCESS' | translate}}</b></div><div ng-if=\"row.branch[col.field] == 'FAILURE'\" class=\"step-state-failure\"><b>{{'FAILURE' | translate}}</b></div><div ng-if=\"row.branch[col.field] != 'SUCCESS' && row.branch[col.field] !='FAILURE'\" class=\"step-state-in-progress\"><b>{{'INPROGRESS' | translate}}</b></div>"
-
-        },
-        {
-            field: "progress",
-            displayName: $scope.status,
-            cellTemplate: "<uib-progressbar class=\"progress\" value=\"row.branch[col.field]\" type=\"success\"><b>{{row.branch[col.field]+\"%\"}}</b></uib-progressbar>"
-        },
-        {
-            cellTemplate: "<div ng-include src=\"'static/frontend/views/undo_redo.html'\"></div>"
-        }
-        ];
-    });
-
-    $scope.myTreeControl = {};
-    $scope.myTreeControl.scope = this;
-    //Undo steps/tasks
-    $scope.myTreeControl.scope.taskStepUndo = function(branch) {
-        $http({
-            method: 'POST',
-            url: branch.url+"undo/"
-        }).then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }, function() {
-            console.log("error");
-        });
-    };
-    //Redo steps/tasks
-    $scope.myTreeControl.scope.taskStepRedo = function(branch){
-        $http({
-            method: 'POST',
-            url: branch.url+"retry/"
-        }).then(function(response) {
-            $timeout(function(){
-                $scope.statusViewUpdate($scope.ip);
-            }, 1000);
-        }, function() {
-            console.log("error");
-        });
-    };
-    $scope.currentStepTask = {id: ""}
-    $scope.myTreeControl.scope.updatePageNumber = function(branch, page) {
-        if(page > branch.page_number && branch.next){
-            branch.page_number = parseInt(branch.next.page);
-            listViewService.getChildrenForStep(branch, branch.page_number);
-        } else if(page < branch.page_number && branch.prev && page > 0) {
-            branch.page_number = parseInt(branch.prev.page);
-            listViewService.getChildrenForStep(branch, branch.page_number);
-        }
-    };
-    //Click on +/- on step
-    $scope.stepClick = function(step) {
-        listViewService.getChildrenForStep(step);
-    };
-
-    //Click funciton for steps and tasks
-    $scope.stepTaskClick = function(branch) {
-        if($scope.stepTaskInfoShow && $scope.currentStepTask.id == branch.id){
-            $scope.stepTaskInfoShow = false;
-        }else {
-            $scope.stepTaskInfoShow = true;
-            $http({
-                method: 'GET',
-                url: branch.url
-            }).then(function(response){
-                $scope.currentStepTask = response.data;
-                if(branch.flow_type == "task"){
-                    $scope.taskInfoModal();
-                } else {
-                    $scope.stepInfoModal();
-                }
-
-            }, function(response) {
-                response.status;
-            });
-        }
-    };
-     // Click funtion columns that does not have a relevant click function
+    // Click funtion columns that does not have a relevant click function
      $scope.ipRowClick = function(row) {
          $scope.selectIp(row);
          if($scope.ip == row){
@@ -191,40 +77,6 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
          $interval.cancel(stateInterval);
          $interval.cancel(listViewInterval);
      });
-     //Get data for status view
-     function checkExpanded(nodes) {
-         var ret = [];
-         nodes.forEach(function(node) {
-             if(node.expanded == true) {
-                 ret.push(node);
-             }
-             if(node.children && node.children.length > 0) {
-                 ret = ret.concat(checkExpanded(node.children));
-             }
-         });
-         return ret;
-     }
-     //Update status view data
-     $scope.statusViewUpdate = function(row){
-         $scope.statusLoading = true;
-         var expandedNodes = [];
-         if($scope.tree_data != []) {
-             expandedNodes = checkExpanded($scope.tree_data);
-         }
-         listViewService.getTreeData(row, expandedNodes).then(function(value) {
-             $scope.tree_data = value;
-             $scope.statusLoading = false;
-         }, function(response){
-             if(response.status == 404) {
-                 $scope.statusShow = false;
-                 $timeout(function(){
-                     $scope.getListViewData();
-                     updateListViewConditional();
-                 }, 1000);
-             }
-         });
-     };
-
      /*******************************************/
      /*Piping and Pagination for List-view table*/
      /*******************************************/
@@ -363,41 +215,8 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
         vm.callServer($scope.tableState);
         $rootScope.loadNavigation(ipSortString);
     };
-    //$scope.getListViewData();
-    //$interval(function(){$scope.getListViewData();}, 5000, false);
 
     //Creates and shows modal with task information
-    $scope.taskInfoModal = function () {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/task_info_modal.html',
-            scope: $scope,
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data, $ctrl) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
-    //Creates and shows modal with step information
-    $scope.stepInfoModal = function () {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/step_info_modal.html',
-            scope: $scope,
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        });
-        modalInstance.result.then(function (data, $ctrl) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
     $scope.max = 100;
     //Get data for eventlog view
     function getEventlogData() {
@@ -747,22 +566,6 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
         "key": "validate_integrity",
     }
     ];
-    //Create and show modal for remove ip
-    $scope.removeIpModal = function (ipObject) {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/remove-ip-modal.html',
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data) {
-            $scope.removeIp(ipObject);
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
-        });
-    }
     //Remove and ip
     $scope.removeIp = function (ipObject) {
         $http({
@@ -776,23 +579,6 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
             $scope.eventShow = false;
             $scope.statusShow = false;
             $rootScope.loadNavigation(ipSortString);
-        });
-    }
-    $scope.tracebackModal = function (profiles) {
-        $scope.profileToSave = profiles;
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'static/frontend/views/task_traceback_modal.html',
-            scope: $scope,
-            size: 'lg',
-            controller: 'ModalInstanceCtrl',
-            controllerAs: '$ctrl'
-        })
-        modalInstance.result.then(function (data) {
-        }, function () {
-            $log.info('modal-component dismissed at: ' + new Date());
         });
     }
     $scope.emailModal = function (profiles) {
@@ -816,8 +602,4 @@ angular.module('myApp').controller('PrepareSipCtrl', function ($log, $uibModal, 
             $scope.submitSip($scope.ip);
         }
     }
-
-    $scope.checkPermission = function(permissionName) {
-        return !angular.isUndefined(PermPermissionStore.getPermissionDefinition(permissionName));
-    };
 });
