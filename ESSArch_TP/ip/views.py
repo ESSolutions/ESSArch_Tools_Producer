f9e3c4fd8a9b7c5e4ac7452a798d117fa4451e7b
Oskar Persson <oskar.persson@polken.se>
2017-09-27 13:22:29 +0200

    Get events for IP using PK

--- ESSArch_TP/ip/views.py
+++ ESSArch_TP/ip/views.py
@@ -313,7 +313,7 @@ class InformationPackageViewSet(viewsets.ModelViewSet):
     @detail_route()
     def events(self, request, pk=None):
         ip = self.get_object()
-        events = filters.OrderingFilter().filter_queryset(request, EventIP.objects.filter(linkingObjectIdentifierValue=ip.object_identifier_value), self)
+        events = filters.OrderingFilter().filter_queryset(request, EventIP.objects.filter(linkingObjectIdentifierValue=ip.pk), self)
         page = self.paginate_queryset(events)
         if page is not None:
             serializers = EventIPSerializer(page, many=True, context={'request': request})
