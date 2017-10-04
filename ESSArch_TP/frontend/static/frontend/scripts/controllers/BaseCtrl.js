53192fac63d60e7eeb5ec44fb7baa74dc850bd58
petrus <petrus.jonsson@gmail.com>
2017-10-04 16:29:19 +0200

    Correctly hide views when IP dissapears from current view

--- ESSArch_TP/frontend/static/frontend/scripts/controllers/BaseCtrl.js
+++ ESSArch_TP/frontend/static/frontend/scripts/controllers/BaseCtrl.js
@@ -189,9 +189,29 @@ angular.module('myApp').controller('BaseCtrl', function (vm, IP, Profile, Step, 
                 tableState.pagination.numberOfPages = result.numberOfPages;//set the number of pages so the pagination can update
                 $scope.ipLoading = false;
                 $scope.initLoad = false;
+                ipExists();
             });
         }
     };
+
+    function ipExists() {
+        if($scope.ip != null) {
+            var temp = false;
+            vm.displayedIps.forEach(function(aic) {
+                if($scope.ip.id == aic.id) {
+                    temp = true;
+                }
+            })
+            if(!temp) {
+                $scope.eventShow = false;
+                $scope.statusShow = false;
+                $scope.filebrowser = false;
+                $scope.requestForm = false;
+                $scope.eventlog = false;
+                $scope.requestEventlog = false;
+            }
+        }
+    }
 
     //Get data for list view
     $scope.getListViewData = function() {
