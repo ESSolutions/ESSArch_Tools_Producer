6d24bd986c1be3e7639384fab3ddc39e0d220d9b
Oskar Persson <oskar.persson@polken.se>
2018-05-30 21:17:37 +0200

    Add route for changing IP organization

--- ESSArch_TP/ip/views.py
+++ ESSArch_TP/ip/views.py
@@ -50,7 +50,8 @@ from rest_framework.response import Response
 from ESSArch_Core.WorkflowEngine.models import ProcessStep, ProcessTask
 from ESSArch_Core.WorkflowEngine.serializers import ProcessStepChildrenSerializer
 from ESSArch_Core.WorkflowEngine.util import create_workflow
-from ESSArch_Core.auth.models import Member
+from ESSArch_Core.auth.models import Group, Member
+from ESSArch_Core.auth.util import get_organization_groups
 from ESSArch_Core.configuration.models import Path
 from ESSArch_Core.exceptions import Conflict
 from ESSArch_Core.ip.filters import InformationPackageFilter
@@ -133,6 +134,22 @@ class InformationPackageViewSet(viewsets.ModelViewSet):
             'responsible__user_permissions', 'responsible__groups__permissions', 'steps',
         ).select_related('submission_agreement')
         return queryset
+
+    @detail_route(methods=['post'], url_path='change-organization')
+    def change_organization(self, request, pk=None):
+        ip = self.get_object()
+        try:
+            org_id = request.data['organization']
+        except KeyError:
+            raise exceptions.ParseError(detail='Missing "organization" parameter')
+
+        try:
+            org = get_organization_groups(request.user).get(pk=org_id)
+        except Group.DoesNotExist:
+            raise exceptions.ParseError('Invalid organization')
+
+        ip.change_organization(org)
+        return Response()
 
     def create(self, request, *args, **kwargs):
         """
